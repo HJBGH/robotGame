1ec4eb50411394bf2027e650ba05db5067de62af
HB <s3529366@student.rmit.edu.au>
2015-10-03 15:13:33 +1000

    Modified command thread to sleep a bit in between operations, creating the illusion of continous movement

--- controller/CommandThread.java
+++ controller/CommandThread.java
@@ -1,9 +1,7 @@
 package controller;
 
 import java.util.ArrayList;
-
 import view.View;
-
 import model.Model;
 
 public class CommandThread implements Runnable
@@ -11,44 +9,65 @@ public class CommandThread implements Runnable
 	private ArrayList<String> commands;
 	private Model the_model;
 	private View the_view;
+
 	
-	public CommandThread(View the_view, Model the_model, ArrayList<String> commands)
+	
+	public CommandThread(View the_view, Model the_model,
+			ArrayList<String> commands)
 	{
 		this.commands = commands;
 		this.the_model = the_model;
 		this.the_view = the_view;
 	}
+
+	
 	
 	public void run()
 	{
-		for(String cmd: commands)
+		for (String cmd : commands)
 		{
-			if((cmd.toUpperCase()).equals("PICK")){
+			
+			if ((cmd.toUpperCase()).equals("PICK"))
+			{
 				the_model.pickup();
-			}
-			else if((cmd.toUpperCase()).equals("DROP")){
+			} 
+			else if ((cmd.toUpperCase()).equals("DROP"))
+			{
 				the_model.drop();
-			}
-			else{
+			} 
+			else
+			{
 				int count = Character.getNumericValue(cmd.charAt(0));
 				char direction = cmd.charAt(1);
-			
-				for(int i = 0; i< count; i++)
+
+				for (int i = 0; i < count; i++)
 				{
 					the_model.moveRobot(direction);
 					try
 					{
-						Thread.sleep(500);
-					}
-					catch (InterruptedException e)
+						Thread.sleep(200);
+					} catch (InterruptedException e)
 					{
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					the_view.redrawBoard(the_model.getBoard());
 				}
 			}
+			
+			try
+			{
+				Thread.sleep(200);
+			} 
+			
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 			the_view.redrawBoard(the_model.getBoard());
 		}
 	}
-	
 }
